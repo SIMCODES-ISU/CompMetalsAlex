161d85f058c2698ddde028eb25c9b339f8577f0f
AlexHaskel <alex.haskel98@myhunter.cuny.edu>
2025-07-28 17:43:24 -0500

    restore tests folder

---
+++ CompMetalsAlex/tests/test_CompMetalsAlex.py
@@ -0,0 +1,15 @@
+"""
+Unit and regression test for the ComputationalModelingMetalAlex package.
+"""
+
+# Import package, test suite, and other packages as needed
+import sys
+
+import pytest
+
+import CompMetalsAlex
+
+
+def test_CompMetalsAlex_imported():
+    """Sample test, will always pass so long as import statement worked."""
+    assert "CompMetalsAlex" in sys.modules
